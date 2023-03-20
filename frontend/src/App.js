a10adfcc188367b5cf85ec8d6407ee6a30a44cca
Fábio Machado de Oliveira <machado2@gmail.com>
2023-03-20 11:28:58 -0300

    prompt history on local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -6,30 +6,50 @@ const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/
 
 console.log(`API_URL: ${API_URL}`)
 
+function FormattedText({ inputTextUsed, generatedText }) {
+  return (
+    <>
+      <b>{inputTextUsed?.toString()}</b>
+      {generatedText?.toString()}
+    </>
+  );
+}
+
 function App() {
   const [inputText, setInputText] = useState('');
+  const [inputTextUsed, setInputTextUsed] = useState('');
   const [generatedText, setGeneratedText] = useState('');
   const [isGenerating, setIsGenerating] = useState(false);
+  const [history, setHistory] = useState(() => {
+    // Load the history from the localStorage
+    const historyJson = localStorage.getItem('chatto-history');
+    return historyJson ? JSON.parse(historyJson) : [];
+  });
+
+  useEffect(() => {
+    // Save the history to the localStorage
+    localStorage.setItem('chatto-history', JSON.stringify(history));
+  }, [history]);
 
   const handleGenerate = async () => {
     setIsGenerating(true);
     setGeneratedText('Generating...');
-  
+
     try {
       const response = await axios.post(API_URL, {
         prompt: inputText
       });
-  
-      const generatedText = response.data.generated_text;
-      const promptIndex = generatedText.indexOf(inputText);
-      const outputText = (
-        <>
-          {generatedText.slice(0, promptIndex)}
-          <b>{generatedText.slice(promptIndex, promptIndex + inputText.length)}</b>
-          {generatedText.slice(promptIndex + inputText.length)}
-        </>
-      );
-      setGeneratedText(outputText);
+
+      const outputText = response.data.generated_text;
+      const promptIndex = outputText.indexOf(inputText);
+      setGeneratedText(outputText.slice(promptIndex + inputText.length).trim())
+      setInputTextUsed(inputText);
+
+      // Add the generated text and prompt to history
+      const newHistoryItem = { prompt: inputText, generatedText };
+      const newHistory = [newHistoryItem, ...history.slice(0, 19)];
+      setHistory(newHistory);
+
     } catch (error) {
       console.error(error);
       setGeneratedText('Error generating text.');
@@ -37,6 +57,10 @@ function App() {
       setIsGenerating(false);
     }
   };
+
+  console.log(`inputTextUsed: ${inputTextUsed}`);
+  console.log(`generatedText: ${generatedText}`);
+
 
   const handleKeyDown = (event) => {
     if (event.ctrlKey && event.key === 'Enter') {
@@ -50,19 +74,40 @@ function App() {
   return (
     <div className="App" style={cursorStyle}>
       <h1>Chatto</h1>
-      <textarea
-        className="input"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-        placeholder="Enter your prompt here"
-        autoFocus
-        onKeyDown={handleKeyDown}
-      />
-      <button className="generateButton" onClick={handleGenerate}>
-        Generate
-      </button>
-      <div className="output">{generatedText}</div>
-    </div>
+      <div className="appcontainer">
+        <div className="history">
+          <div className="historylistcontainer">
+            <ul>
+              {history.map((item, index) => (
+                <li key={index} onClick={() => {
+                  setInputText(item.prompt);
+                  setGeneratedText(item.generatedText);
+                }}>
+                  <FormattedText inputTextUsed={item?.prompt} generatedText={item?.generatedText} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <button className='button' onClick={() => setHistory([])}>Clear History</button>
+        </div>
+        <div className="generationcontainer">
+          <textarea
+            className="input"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Enter your prompt here"
+            autoFocus
+            onKeyDown={handleKeyDown}
+          />
+          <button className="button" onClick={handleGenerate}>
+            Generate
+          </button>
+          <div className="output">
+            <FormattedText inputTextUsed={inputTextUsed} generatedText={generatedText} />
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
